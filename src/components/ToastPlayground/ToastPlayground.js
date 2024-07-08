5656c982b466df51eb3e458e8e081d37ec0564e8
Anna Rosca <hello@annarosca.com>
2024-07-08 12:19:03 +0100

    ex 2, added basic functionality to Toast, form control and dismiss

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import Button from "../Button";
 
+import Toast from "../Toast/Toast";
+
 import styles from "./ToastPlayground.module.css";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
     const [message, setMessage] = React.useState("");
-    const [variant, setVariant] = React.useState("notice");
+    const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+    const [toastVisible, setToastVisible] = React.useState(false);
 
     return (
         <div className={styles.wrapper}>
@@ -16,7 +19,15 @@ function ToastPlayground() {
                 <img alt="Cute toast mascot" src="/toast.png" />
                 <h1>Toast Playground</h1>
             </header>
-
+            {toastVisible && (
+                <Toast
+                    message={message}
+                    variant={variant}
+                    onDismiss={() => {
+                        setToastVisible(false);
+                    }}
+                />
+            )}
             <div className={styles.controlsWrapper}>
                 <div className={styles.row}>
                     <label
@@ -66,7 +77,13 @@ function ToastPlayground() {
                     <div
                         className={`${styles.inputWrapper} ${styles.radioWrapper}`}
                     >
-                        <Button>Pop Toast!</Button>
+                        <Button
+                            onClick={() => {
+                                setToastVisible(true);
+                            }}
+                        >
+                            Pop Toast!
+                        </Button>
                     </div>
                 </div>
             </div>
